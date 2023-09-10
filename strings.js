e02fe9a275e8c8c9ebaf030ac53f83eeb9ab0ffc
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-09-10 07:46:39 +0000

    implemented string methods

---
+++ strings.js
@@ -0,0 +1,11 @@
+let name = "akash";
+let age = 19;
+
+console.log("Hello " + name+" your age is "+ age); // bad way of programming
+
+console.log(`Hello ${name} you age is ${age}`); // good way f programming
+
+//Another way of declarinf string
+
+let data = new String('Hello world');
+console.log(data[1]);
