392bf21eaad83f91ecd31f683c12461dcad1c02d
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-09-10 06:31:53 +0000

    String to number conversion

---
+++ var.js
@@ -0,0 +1,8 @@
+// console.log("Hello World")
+
+console.log("1"+ 2);  // 12
+console.log(1+ "2");  // 12
+console.log("1"+ 2 + 2);  // 122
+console.log(1+ 2 + "2");  // 32
+
+// This type of code should be used in production
