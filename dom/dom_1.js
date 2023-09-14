268c9150b71e143af31abbc778edeb38dcdcd4ce
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-09-14 10:54:43 +0000

    Learning dom properties

---
+++ dom/dom_1.js
@@ -0,0 +1,12 @@
+const test = document.getElementById('heading')
+
+//test.style.backgroundColor=('red')
+//test.textContent  //output : DOM learning on chai aur code
+//test.innerHTML    //output : DOM learning on chai aur code
+//test.innerText    //output : DOM learning on chai aur code
+
+
+/////////////////////////////////// after we span tag in our h1 tag and set display to none /////////////////////////////
+
+//test.innerText    //output : DOM learning on chai aur code             // Displays only visible text
+//test.textContent  //output : DOM learning on chai aur code test text   //Displays all text 
