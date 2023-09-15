78b6ae10b69a120a9144547521c7fd28b77c4913
Akash <57212963+Akash-152000@users.noreply.github.com>
2023-09-15 14:10:24 +0530

    Implemented project 5

---
+++ projects/Project5/project_5.js
@@ -0,0 +1,26 @@
+const randomColor = function(){
+    const hex = '0123456789ABCDEF'
+    let color = '#'
+    for(let i = 0;i< 6;i++){
+        color+=hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+
+
+let intervalId;
+
+const changeBgColor = function(){
+    document.body.style.backgroundColor = randomColor()
+}
+
+document.querySelector('#start').addEventListener('click',function(){
+    if(!intervalId){
+        intervalId = setInterval(changeBgColor,1000) 
+    }
+})
+document.querySelector('#stop').addEventListener('click',function(){
+    clearInterval(intervalId)
+    intervalId = null;
+
+})
